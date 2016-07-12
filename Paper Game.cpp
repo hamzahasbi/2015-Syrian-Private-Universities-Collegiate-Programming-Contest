eb349b6228e4ca52683f9d76ffa38e35c08aee8a
Hasbi Hamza <hamza.hasbi@gmail.com>
2016-07-12 21:24:21 +0100

    Update Paper Game.cpp

--- Paper Game.cpp
+++ Paper Game.cpp
@@ -1,50 +1,26 @@
 /*
-	Author: Anass BENDARSI 
-	Date: 16/06/2016 16:29:38
+	Author: Hamza Hasbi
+	Date: 15-Jun-16 12:16:53 PM
 */
 #include<bits/stdc++.h>
 #define ll long long
 #define ld long double
 using namespace std;
-int main(){
+main()
+{
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
+    cin.tie(NULL);
 	int t;
 	cin>>t;
-	for(int k=1;k<=t;k++){
-		string mot;
-		int tab[26]={0};
-		cin>>mot;
-		for(int i=0;i<mot.size();i++){
-			tab[mot[i]-'a']++;	
-		}
-		/*for(int i=0;i<26;i++){
-			cout<<tab[i]<<" ";
-		}
-		cout<<endl;*/
-		int c=0;
-		for(int i=0;i<26;i++){
-			if(tab[i]%2==1) c++;
-		}
-		if(c>1) cout<<"Case "<<k<<": "<<"impossible"<<endl;
-		else{
-			string str="",str2="";
-			int indice=-1;
-			for(int i=0;i<26;i++){
-				if(tab[i]%2==1) indice=i;
-				for(int j=0;j<tab[i]/2;j++){
-					str+='a'+i;
-				}
-			}
-			for(int i=str.size()-1;i>=0;i--){
-				str2+=str[i];
-			}
-			if(indice>=0)str+='a'+indice;
-			str+=str2;
-		 cout<<"Case "<<k<<": "<<str<<endl;
-		}
+	while(t--)
+	{
+		int x,y;
+		bool check;
+		cin>>x>>y;
+		((x*y)%2==0)? check=true: check=false ;
+		if(check) puts("Hasan");
+		else puts("Hussain");
 	}
-
-	return 0;
+	cout << flush;
+  return 0;
 }
